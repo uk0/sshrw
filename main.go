9c8cb1340812cf6f441bb1430f8fbd9ba3833972
Unknow 0 :) <zhangjianxinnet@gmail.com>
2022-10-30 15:14:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,14 +6,8 @@ import (
 	"os"
 	"strings"
 )
-
 func main() {
-
 	cnf :=GetConfig()
-	//fmt.Println(cnf.Config.Tunnel)
-	//fmt.Println(cnf.Config.Destinations)
-	//fmt.Println(cnf.Config.Password)
-
 	for _,dstsrc :=range cnf.Config.Destinations {
 		dst1Src :=strings.Split(dstsrc,"->")
 		// Setup the tunnel, but do not yet start it yet.
@@ -32,12 +26,6 @@ func main() {
 		// You can provide a logger for debugging, or remove this line to
 		// make it silent.
 		tunnel.Log = log.New(os.Stdout, "", log.Ldate | log.Lmicroseconds)
-		// Start the server in the background. You will need to wait a
-		// small amount of time for it to bind to the localhost port
-		// before you can start sending connections.
-
-		//println(_dst_1_src[0])
-		//println(_dst_1_src[1])
 
 		go tunnel.Start()
 
